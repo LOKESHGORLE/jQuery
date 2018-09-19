d4c9ca5b0c46e2c880d94290981f820355cb9e52
LOKESHGORLE <43067941+LOKESHGORLE@users.noreply.github.com>
2018-09-19 12:35:15 +0530

    basic index
    html basic elements,
     jquery commands to call by selector

---
+++ index/index/appl.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(BodyBackroundChange())
+
+
+
+function BodyBackroundChange() {
+    $("#submit").click.css("background", "red");
+}
+
+$(document).ready(function () {
+    $("#btnhello").click("alerthello");
+    function alerthello() {
+        alert("hello");
+    }
+})
+
+
+/*$("#Qualification").css("backgroung",'red');*/
+
+function AddQualificationColumn() {
+    var form = document.getElementById('AddQualification');
+    var addform = document.createElement("input")
+    console.log('start');
+    form.appendChild(addform)
+    console.log('created');
+}
+
+
+//function createQualField() {
+//    var input = document.createElement('input');
+//    input.type = 'text';
+//    input.name = 'pet[]';
+//    return input;
+//}
+
+
+//document.getElementById('Qualification').addEventListener('click', function (e) {
+//    form.appendChild(createQualField())
+//    ;
+//});
+
+//document.getElementById("AddQualification").onclick = function () {
+//    var form = document.getElementById('AddQualification');
+//    var addform = document.createElement("input")
+//    form.appendChild(addform)
+//}
